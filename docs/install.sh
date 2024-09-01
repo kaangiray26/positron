6da1897efc2ab17d182cdb2f126ddf1c8aeee281
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2024-09-01 02:53:07 +0200

    README

--- docs/install.sh
+++ docs/install.sh
@@ -6,20 +6,20 @@
 url=$(curl -s https://api.github.com/repos/kaangiray26/positron/releases/latest | grep "browser_download_url" | cut -d '"' -f 4)
 
 # Download the release
-wget -O positron.tar.gz $url
+wget -q -O positron.tar.gz $url
 
 # Open the tar archive
-tar -xzf positron.tar.gz
+tar -qxzf positron.tar.gz
 
 # Remove the tar archive
-rm positron.tar.gz
+rm -f positron.tar.gz
 
 # Create virtual environment
 python3 -m venv env
 
 # Install dependencies
-env/bin/python3 -m pip install --upgrade pip
-env/bin/python3 -m pip install -r requirements.txt
+env/bin/python3 -m pip -q install --upgrade pip
+env/bin/python3 -m pip -q install -r requirements.txt
 
 echo "Positron installed successfully!"
 echo "Run 'env/bin/positron --help' to get started."
